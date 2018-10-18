41ca246e87c5bea3f1eb900f30ec2aa7a8de5dbe
Jaidev Anoop <jaidev.anoop@gmail.com>
2018-10-18 16:38:34 +0530

    Struct in Struct

--- structs/main.go
+++ structs/main.go
@@ -2,18 +2,35 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCope int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	cInfo     contactInfo
 }
 
 func main() {
 	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	fmt.Printf("%v\n", alex)
 
 	var bob person
 	bob.firstName = "Bob"
 	bob.lastName = "Anderson"
 
-	fmt.Println(bob)
+	fmt.Printf("%+v\n", bob)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		cInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCope: 94000,
+		},
+	}
+
+	fmt.Printf("%+v\n", jim)
 }
